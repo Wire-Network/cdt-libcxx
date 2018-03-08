9a2c03a6dbe4deaa1eceb012ada6795ac526cf4a
Anton Perkov <anton.perkov@block.one>
2018-03-08 16:26:54 -0500

    floating point functions restored

--- include/math.h
+++ include/math.h
@@ -191,8 +191,8 @@ float          fdimf(float x, float y);
 long double    fdiml(long double x, long double y);
 
 floating_point fma (arithmetic x, arithmetic y, arithmetic z);
-float          fmaf(float x, float y, float z);
-long double    fmal(long double x, long double y, long double z);
+//float          fmaf(float x, float y, float z);
+//long double    fmal(long double x, long double y, long double z);
 
 floating_point fmax (arithmetic x, arithmetic y);
 float          fmaxf(float x, float y);
@@ -308,7 +308,7 @@ extern "C++" {
 
 #include <type_traits>
 #include <limits>
-   /*
+
 // signbit
 
 #ifdef signbit
@@ -1162,7 +1162,7 @@ fdim(_A1 __lcpp_x, _A2 __lcpp_y) _NOEXCEPT
 }
 
 // fma
-
+/*
 inline _LIBCPP_INLINE_VISIBILITY float       fma(float __lcpp_x, float __lcpp_y, float __lcpp_z) _NOEXCEPT                   {return ::fmaf(__lcpp_x, __lcpp_y, __lcpp_z);}
 inline _LIBCPP_INLINE_VISIBILITY long double fma(long double __lcpp_x, long double __lcpp_y, long double __lcpp_z) _NOEXCEPT {return ::fmal(__lcpp_x, __lcpp_y, __lcpp_z);}
 
@@ -1183,7 +1183,7 @@ fma(_A1 __lcpp_x, _A2 __lcpp_y, _A3 __lcpp_z) _NOEXCEPT
                      std::is_same<_A3, __result_type>::value)), "");
     return ::fma((__result_type)__lcpp_x, (__result_type)__lcpp_y, (__result_type)__lcpp_z);
 }
-
+*/
 // fmax
 
 inline _LIBCPP_INLINE_VISIBILITY float       fmax(float __lcpp_x, float __lcpp_y) _NOEXCEPT             {return ::fmaxf(__lcpp_x, __lcpp_y);}
@@ -1481,7 +1481,7 @@ template <class _A1>
 inline _LIBCPP_INLINE_VISIBILITY
 typename std::enable_if<std::is_integral<_A1>::value, double>::type
 trunc(_A1 __lcpp_x) _NOEXCEPT {return ::trunc((double)__lcpp_x);}
-*/
+
 } // extern "C++"
 
 #endif // __cplusplus
